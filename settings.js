c654331ce125c9553ad048300bc3fdc6723000b4
ZexxaBot <134960384+ZexxaBot@users.noreply.github.com>
2023-05-30 03:01:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'ZexxaDEV'// yourname
 global.MONGO_DB_URI = "mongodb+srv://zexxadev:rhpulVsiwQu95nhV@cluster0.prfpwmz.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "mhmmd.fauzi12321@gmail.com" //email
-global.email_password = "besnkexncvztikww" //application password email
+global.email_password = "qoej cytj rddv jytm" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
